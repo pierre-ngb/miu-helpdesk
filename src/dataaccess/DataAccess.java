b190236b8e10635e816540e320dbec905cee9a70
Pierrot <pngimbidi@miu.edu>
2021-03-03 17:34:24 -0600

    Methods for Data access layer done. Not yet implemented

--- src/dataaccess/DataAccess.java
+++ src/dataaccess/DataAccess.java
@@ -1,5 +1,18 @@
 package dataaccess;
 
+import java.util.List;
+import business.*;
+
 public interface DataAccess {
 
+	public List<User> allUsers();
+	
+	public User login(String username, String pw);
+	
+	public List<Ticket> getAllTickets();
+	
+	public void saveTicket(Ticket t);
+	
+	public void updateStatus(Status st);
+	
 }
--- src/dataaccess/DataAccessFacade.java
+++ src/dataaccess/DataAccessFacade.java
@@ -1,6 +1,40 @@
 package dataaccess;
 
-public class DataAccessFacade {
+import java.util.List;
+
+import business.Ticket;
+
+public class DataAccessFacade implements DataAccess{
+
+	@Override
+	public List<User> allUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User login(String username, String pw) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Ticket> getAllTickets() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveTicket(Ticket t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateStatus(Status st) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	
 }
