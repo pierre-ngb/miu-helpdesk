ff2177357e3317563773e0daac54095b778aea23
Boaz Turyahikayo <bturyahikayo@miu.edu>
2021-03-03 17:56:02 -0600

    Edited ticket class

--- src/business/Ticket.java
+++ src/business/Ticket.java
@@ -1,8 +1,14 @@
 package business;
 
 public class Ticket {
-
-	public Ticket() {
+	String title;
+	String description;
+	Client client;
+	
+	public Ticket(String title,	String description,Client c) {
+		this.title = title;
+		this.description = description;
+		this.client = c;
 		
 	}
 }
