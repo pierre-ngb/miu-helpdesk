9d5b3ad41ea8fb0a31285e2ff12bcd539ca60972
Boaz Turyahikayo <bturyahikayo@miu.edu>
2021-03-03 18:04:51 -0600

    updated the ticket class

--- src/business/Ticket.java
+++ src/business/Ticket.java
@@ -4,11 +4,27 @@ public class Ticket {
 	String title;
 	String description;
 	Client client;
-	
+	Agent agent ;
+	Manager manager;
+	Status status;
+	String solution;
 	public Ticket(String title,	String description,Client c) {
 		this.title = title;
 		this.description = description;
-		this.client = c;
-		
+		this.client = c;		
 	}
+	
+	void setAgent(Agent a) {
+		agent = a;
+	}
+	void setManager(Manager m) {
+		manager = m ;
+	}
+	void setStatus(Status s) {
+		status =s;
+	}
+	void setSolution(String solution) {
+		this.solution = solution;
+	}
+	
 }
