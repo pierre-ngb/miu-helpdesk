3eb2bfe5fd6bfcafceebe0aba7a3ad837d5f00c1
Boaz Turyahikayo <bturyahikayo@miu.edu>
2021-03-03 21:53:41 -0600

    dd

--- src/business/User.java
+++ src/business/User.java
@@ -7,5 +7,47 @@ public abstract class User {
 	private String userName;
 	private String password;
 	
+	public User() {}
+	
+	public User(String f, String l, String u, String p) {
+		firstName = f;
+		lastName = l;
+		userName = u;
+		password = p;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 
 }
